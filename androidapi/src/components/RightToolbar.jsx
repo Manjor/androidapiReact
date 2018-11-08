6a44a9c2817d3bcc742e831daf542700abc6062b
manjor <manoeltavares.jr@gmail.com>
2018-11-08 14:13:24 -0300

    Create component RightToolbar

---
+++ androidapi/src/components/RightToolbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './RightToolbar.css'
+import Link from '../components/Link'
+
+export default props=>
+    <div className='toolbar'>
+        <h6 className='bg-dark p-2 shadow text-white'>Apis</h6>
+        <Link name='LottieApi' url='#'/>
+        <Link name='Google Maps' classe='disabled' url='#'/>
+        <Link name='Youtube V3' classe='disabled' url='#'/>
+        <Link name='The Movie DB'  url='#'/>
+        <Link name='Ingresso.com' classe='disabled' url='#'/>
+        <Link name='Spotify' classe='disabled' url='#'/>
+        <Link name='Instagram' classe='disabled' url='#'/>
+        <Link name='SoudCloud' classe='disabled' url='#'/>
+        <h6 className='bg-dark p-2 shadow text-white'>Bibliotécas</h6>
+        <Link name='Picasso' url='#'/>
+        <Link name='Gson' url='#'/>
+        <Link name='OkHttp' url='#'/>
+        <Link name='Retrofit' classe='disabled' url='#'/>
+    </div>
--- androidapi/src/main/App.jsx
+++ androidapi/src/main/App.jsx
@@ -4,10 +4,12 @@ import 'font-awesome/css/font-awesome.min.css'
 import Nav from '../template/Nav'
 import Main from '../template/Main'
 import Footer from '../template/Footer'
+import RightToolbar from '../components/RightToolbar';
 
 export default props =>
     <div>
         <Nav/>
+        <RightToolbar />
         <Main/>
         <Footer/>
     </div>
